744240823b932523be3ab7aa0b63314980260de9
savagethug01 <blueharry39@gmail.com>
2025-07-05 02:42:26 -0700

    Update build.sh
    Save

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env bash
-# Exit on error
+#!/bin/bash
 set -o errexit
 
-# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --no-input
